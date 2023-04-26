409fc688a546f8ec152065592dbbaa110ccfc09e
DavidJMS <david.marabay@gmail.com>
2023-04-26 15:30:51 -0400

    Setting db

--- backend/settings.py
+++ backend/settings.py
@@ -105,9 +105,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+    },
 }
 
 
